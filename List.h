71cfa07345e36c1dad9e0f9354e1ae9fb600715d
wujun <819688700@qq.com>
2019-06-08 21:57:19 +0800

    hide class

--- List.h
+++ List.h
@@ -1,28 +1,30 @@
 #pragma once
 
-//链表结构体类
-class Link
-{
-public:
-	Link(Link *pNext, int id) : _pNext(pNext), _id(id) {}
-    //内联函数，提高效率
-	int Id() const { return _id; }
-	Link * Next() const { return _pNext; } 
-private:
-	int  _id;
-	Link *_pNext;
-};
-
 //////////////////////////////////////////////////////////////////////////
 
 class List
 {
+    friend class ListSeq;
 public:
 	List() : _pHead(0) {}
 	~List(void);
 	void Add(int id);
-	Link const * GetHead() const { return _pHead; }
+
+    // 链表结构体类： 嵌套类，隐藏实现细节
+    class Link
+    {
+    public:
+        Link(Link *pNext, int id) : _pNext(pNext), _id(id) {}
+        //内联函数，提高效率
+        int Id() const { return _id; }
+        Link * Next() const { return _pNext; }
+    private:
+        int  _id;
+        Link *_pNext;
+    };
+
 private:
+    Link const * GetHead() const { return _pHead; }
 	Link *_pHead;  //头指针
 };
 
@@ -31,12 +33,15 @@ private:
 class ListSeq
 {
 public:
-    ListSeq(List const & list)
-        : _pLink(list.GetHead()) {}
     bool AtEnd() const { return _pLink==0; }
     void Advance() { _pLink = _pLink->Next(); }
     int GetId() const { return _pLink->Id(); }
+protected:
+    //对于protected的函数，子类的“内部”的其他函数可以调用之
+    ListSeq(List const & list)
+        : _pLink(list.GetHead()) {}
 
 private:
-    Link const * _pLink;
+    //使用嵌套类需要加外部类
+    List::Link const * _pLink;
 };
